90cc7a025ae8bc038bc0f0e91470bcf3071043e1
drunkel <dylan.runkel@gmail.com>
2013-10-28 21:11:56 -0300

    added actionbar to switch to camera activity

--- src/com/edu/seenit/MainActivity.java
+++ src/com/edu/seenit/MainActivity.java
@@ -1,8 +1,11 @@
 package com.edu.seenit;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -14,9 +17,31 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	    // Inflate the menu items for use in the action bar
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main, menu);
+	    return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.action_camera:
+	            openSearch();
+	            return true;
+	        case R.id.action_settings:
+	           // openSettings();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+	
+	/** Called when the user clicks the Camera button */
+	public void openSearch() {
+	    Intent intent = new Intent(this, Camera.class);
+	    startActivity(intent);
 	}
 
 }
